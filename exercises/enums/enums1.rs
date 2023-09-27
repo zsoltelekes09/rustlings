1eb5a0455e1ed2442666a57ffc94aa4be7ae37bf
zsoltelekes <zsoltelekes09@gmail.com>
2023-09-27 20:13:13 +0200

    Add rustlings to git

---
+++ exercises/enums/enums1.rs
@@ -0,0 +1,16 @@
+// enums1.rs
+//
+// No hints this time! ;)
+
+
+#[derive(Debug)]
+enum Message {
+   Quit,Echo,Move,ChangeColor
+}
+
+fn main() {
+    println!("{:?}", Message::Quit);
+    println!("{:?}", Message::Echo);
+    println!("{:?}", Message::Move);
+    println!("{:?}", Message::ChangeColor);
+}
